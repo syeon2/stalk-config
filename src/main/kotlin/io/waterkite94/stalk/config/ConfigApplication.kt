177661cdc78fbb6cc72482afbadc4ac740c6e1d0
SUYEON KIM <waterkite94@gmail.com>
2024-07-17 17:20:24 +0900

    [Chore] set up ktlint

--- src/main/kotlin/io/waterkite94/stalk/config/ConfigApplication.kt
+++ src/main/kotlin/io/waterkite94/stalk/config/ConfigApplication.kt
@@ -2,7 +2,9 @@ package io.waterkite94.stalk.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class ConfigApplication
 
